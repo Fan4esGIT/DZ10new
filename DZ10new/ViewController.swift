9e00f76758b9d0c3ec219d3537dbca79137ff64d
Fan4es <skairasing@yandex.ru>
2021-06-28 13:30:20 +0500

    Initial Commit

---
+++ DZ10new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ10new
+//
+//  Created by FanisSalmanov on 28.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
